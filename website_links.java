62115be2083ea54000fb3b2c8cf9e21f1b69a398
Dsa037 <107195757+Dsa037@users.noreply.github.com>
2022-06-10 11:58:51 +0500

    Add files via upload

---
+++ website_links.java
@@ -0,0 +1,17 @@
+package sotring;
+
+public class website_links {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * 
+		 https://www.javatpoint.com/dijkstra-algorithm-java#:~:text=Step1%3A%20All%
+		  20nodes%20should%20be,node%20as%20the%20current%20node
+		 * 
+		 */
+
+	}
+
+}
